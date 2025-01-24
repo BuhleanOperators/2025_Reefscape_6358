fcddaf87880c5355b4a5785037419aa17ca5f2b4
Max Young <maxyoung214@gmail.com>
2025-01-24 14:52:03 -0700

    Minor changes for log clarity

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,37 +1,44 @@
-// Copyright 2021-2025 FRC 6328
+// Copyright (c) 2025 FRC 6328
 // http://github.com/Mechanical-Advantage
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// version 3 as published by the Free Software Foundation or
-// available in the root directory of this project.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.RobotBase;
-
-/**
- * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
- * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
- * (log replay from a file).
- */
+/** This class contains global configuration describing the current robot and runtime mode. */
 public final class Constants {
-  public static final Mode simMode = Mode.REAL;
-  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+  public static final double loopPeriodSecs = 0.02;
+  public static final boolean tuningMode = false;
+  public static final Mode currentMode = Mode.REAL;
 
-  public static enum Mode {
+  public enum Mode {
     /** Running on a real robot. */
     REAL,
 
-    /** Running a physics simulator. */
-    // SIM,
-
     /** Replaying from a log file. */
     REPLAY
+  }
+
+  public enum RobotType {
+    DEVBOT,
+    COMPBOT
+  }
+
+  public static boolean disableHAL = false;
+
+  public static void disableHAL() {
+    disableHAL = true;
+  }
+
+  /** Checks that the default robot is selected and tuning mode is disabled. */
+  public static class CheckPullRequest {
+    public static void main(String... args) {
+      if (tuningMode) {
+        System.err.println("Do not merge, non-default constants are configured.");
+        System.exit(1);
+      }
+    }
   }
 }
