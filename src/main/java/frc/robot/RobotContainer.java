a9a70dc55bf766bf3248b4b9ea72bb38b82c5b30
Max Young <maxyoung214@gmail.com>
2025-03-27 09:30:23 -0600

    Auto before Comp

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,15 +15,19 @@ package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.events.EventTrigger;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants.Height;
 import frc.robot.commands.DriveCommands;
@@ -42,6 +46,9 @@ import frc.robot.subsystems.drive.ModuleIOTalonFX;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.elevator.ElevatorIO;
 import frc.robot.subsystems.elevator.ElevatorIONeo;
+import frc.robot.subsystems.led.Led;
+
+import java.util.stream.Stream;
 
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
@@ -57,6 +64,7 @@ public class RobotContainer {
   private final Coral coral;
   private final Elevator elevator;
   private final Algae algae;
+  private final Led leds = Led.getInstance();
 
   // Controller
   private final CommandXboxController xDriver = new CommandXboxController(0);
@@ -105,47 +113,66 @@ public class RobotContainer {
 
     //----- Set up Named Commands -----
     //Elevator Commands
-    // NamedCommands.registerCommand("ElevatorHome", 
-    //     Commands.runOnce(() -> elevator.setPosition(Height.HOME), elevator));
-    // NamedCommands.registerCommand("ElevatorL2", 
-    //     Commands.runOnce(() -> elevator.setPosition(Height.L2), elevator));
-    // NamedCommands.registerCommand("ElevatorL3", 
-    //     Commands.runOnce(() -> elevator.setPosition(Height.L3), elevator));
-    // NamedCommands.registerCommand("ElevatorHighAlgae", 
-    //     Commands.runOnce(() -> elevator.setPosition(Height.HIGH_ALGAE), elevator));
-    // NamedCommands.registerCommand("ElevatorLowAlgae", 
-    //     Commands.runOnce(() -> elevator.setPosition(Height.LOW_ALGAE), elevator));
-    //Scoring Commands
-    // NamedCommands.registerCommand("CoralTroughScore",
-    //     Commands.run(() -> coral.scoreTrough()).withTimeout(0.25));
-    // NamedCommands.registerCommand("CoralBranchScore", 
-    //     Commands.run(() -> coral.scoreCoral()).withTimeout(0.25));
-    //Algae Commands
-    // NamedCommands.registerCommand("RemoveAlgae", 
-    //     Commands.run(() -> algae.extakeAlgae()).withTimeout(2));
-    // NamedCommands.registerCommand("GrabAlgae", 
-    //     Commands.run(() -> algae.extakeAlgae()).withTimeout(1));  
-    // NamedCommands.registerCommand("SpitAlgae", 
-    //     Commands.run(() -> algae.extakeAlgae)).withTimeout(1);
+    NamedCommands.registerCommand("ElevatorHome", 
+        Commands.runOnce(() -> elevator.setPosition(Height.HOME), elevator));
+    NamedCommands.registerCommand("ElevatorL2", 
+        Commands.runOnce(() -> elevator.setPosition(Height.L2), elevator));
+    NamedCommands.registerCommand("ElevatorL3", 
+        Commands.runOnce(() -> elevator.setPosition(Height.L3), elevator));
+    NamedCommands.registerCommand("ElevatorHighAlgae", 
+        Commands.runOnce(() -> elevator.setPosition(Height.HIGH_ALGAE), elevator));
+    // Scoring Commands
+    NamedCommands.registerCommand("CoralTroughScore",
+        Commands.runEnd(() -> coral.scoreTrough(), coral::stop).withTimeout(0.3));
+    NamedCommands.registerCommand("CoralBranchScore", 
+        Commands.runEnd(() -> coral.scoreCoral(), coral::stop).withTimeout(0.35));
+    NamedCommands.registerCommand("LeftCoralTroughScore", 
+        Commands.runEnd(() -> coral.setSpeeds(0.1, 0.4), coral::stop).withTimeout(0.35));
+    NamedCommands.registerCommand("RightCoralTroughScore", 
+        Commands.runEnd(() -> coral.setSpeeds(0.4, 0.1), coral::stop).withTimeout(0.35));
+    // Algae Commands
+    NamedCommands.registerCommand("EjectAlgae", 
+        Commands.runEnd(() -> algae.extakeAlgae(), algae::stop).withTimeout(2));
+    NamedCommands.registerCommand("GrabAlgae", 
+        Commands.runEnd(() -> algae.intakeAlgae(), algae::stop).withTimeout(1));  
+
+    new EventTrigger("RunAlgae").whileTrue(Commands.run(() -> algae.intakeAlgae()).withTimeout(2));
+    new EventTrigger("HoldAlgae").whileTrue(Commands.run(() -> algae.holdAlgae()));
 
     // Set up auto routines
-    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+    autoChooser = new LoggedDashboardChooser<>("Auto Choices", 
+        AutoBuilder.buildAutoChooserWithOptionsModifier(
+            (stream) -> Constants.isCompetition ? stream.filter(auto -> auto.getName().startsWith("comp")) : stream));
 
     // Set up SysId routines
+    // autoChooser.addOption(
+    //     "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+    // autoChooser.addOption(
+    //     "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
+    // autoChooser.addOption(
+    //     "Drive SysId (Quasistatic Forward)",
+    //     drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    // autoChooser.addOption(
+    //     "Drive SysId (Quasistatic Reverse)",
+    //     drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    // autoChooser.addOption(
+    //     "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+    // autoChooser.addOption(
+    //     "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
     autoChooser.addOption(
-        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+        "Simple Center", new PathPlannerAuto("Simple Center"));
     autoChooser.addOption(
-        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
+        "Left Sabotage", new PathPlannerAuto("Sabotage"));
     autoChooser.addOption(
-        "Drive SysId (Quasistatic Forward)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+        "Right Sabotage", new PathPlannerAuto("Sabotage", true));
     autoChooser.addOption(
-        "Drive SysId (Quasistatic Reverse)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+        "LeftL2L2", new PathPlannerAuto("LeftL2L2"));
     autoChooser.addOption(
-        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+        "RightL2L2", new PathPlannerAuto("LeftL2L2", true));
     autoChooser.addOption(
-        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+            "LeftL2L1L1", new PathPlannerAuto("LeftL2L1L1"));
+    autoChooser.addOption(
+            "RightL2L1L1", new PathPlannerAuto("LeftL2L1L1", true));
 
     // Configure the button bindings
     configureButtonBindings();
@@ -183,6 +210,13 @@ public class RobotContainer {
         .whileTrue(
             Commands.startEnd(
                 () -> coral.run(), coral::stop, coral));
+    
+    //Run coral back in
+    xDriver
+        .rightBumper()
+        .whileTrue(
+            Commands.startEnd(
+                () -> coral.reverse(), coral::stop, coral));
 
     //Extake Algea
     xDriver
@@ -197,14 +231,22 @@ public class RobotContainer {
         .whileTrue(
             Commands.startEnd(
                 () -> algae.intakeAlgae(), algae::stop, algae));
+    
+    //Human Player Attention Alert
+    xDriver
+        .x()
+        .whileTrue(
+            Commands.startEnd(
+                () -> leds.hpAttentionAlert = true, () -> leds.hpAttentionAlert = false));
 
+    //----- CoPilot Controls -----
     //Run elevator to hight for L1 / Coral station
-    // coPilot
-    //     .b()
-    //     .onTrue(
-    //         Commands.run(
-    //             () -> 
-    //                 elevator.setPosition(Height.HOME), elevator));
+    coPilot
+        .b()
+        .onTrue(
+            Commands.run(
+                () -> 
+                    elevator.setPosition(Height.HOME), elevator));
     
     //Run elevator to height for L2
     coPilot
@@ -215,21 +257,30 @@ public class RobotContainer {
                     elevator.setPosition(Height.L2), elevator));
 
     //Run elevator to height for L3
-    // coPilot
-    //     .x()
-    //     .onTrue(
-    //         Commands.run(
-    //             () -> 
-    //                 elevator.setPosition(Height.L3), elevator));
-    
-    //Run elevator to height for high algae
     coPilot
-        .rightBumper()
+        .x()
+        .onTrue(
+            Commands.run(
+                () -> 
+                    elevator.setPosition(Height.L3), elevator));
+
+    //Run elevator to height for High alage
+    coPilot
+         .rightBumper()
         .onTrue(
             Commands.run(
                 () -> 
                     elevator.setPosition(Height.HIGH_ALGAE), elevator));
 
+    new Trigger(
+        () -> 
+            DriverStation.isTeleopEnabled()
+                && DriverStation.getMatchTime() >0
+                && DriverStation.getMatchTime() <= Math.round(15.0))
+        .onTrue(
+            controllerRumbleCommand().withTimeout(0.5)
+            .beforeStarting(() -> leds.endgameAlert = true)
+            .finallyDo(() -> leds.endgameAlert = false));
   }
 
   /**
@@ -239,6 +290,7 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     return autoChooser.get();
+    // return null;
   }
 
   public void initPreferences(){
@@ -246,9 +298,9 @@ public class RobotContainer {
     Preferences.initDouble("L3 Height", 24.5);
     Preferences.initDouble("High Algae Height", 19);
     Preferences.initDouble("L2 & L3 Scoring Speed", 0.45);
-    Preferences.initDouble("High Algae Height", 24.5);
     Preferences.initDouble("Trough Left Speed", 0.15);
     Preferences.initDouble("Trough Right Speed", 0.45);
+    Preferences.initDouble("Coral Reverse Speed", 0.1);
     Preferences.initDouble("Algae Intake Speed", 1.0);
     Preferences.initDouble("Algae Extake Speed", -1.0);
   }
@@ -256,8 +308,10 @@ public class RobotContainer {
     Preferences.getDouble("L2 Height", 10);
     Preferences.getDouble("L3 Height", 24.5);
     Preferences.getDouble("High Algae Height", 19);
+    Preferences.getDouble("L2 & L3 Scoring Speed", 0.45);
     Preferences.getDouble("Trough Left Speed", 0.15);
     Preferences.getDouble("Trough Right Speed", 0.45);
+    Preferences.getDouble("Coral Reverse Speed", 0.1);
     Preferences.getDouble("Algae Intake Speed", 1.0);
     Preferences.getDouble("Algae Extake Speed", -1.0);
 
@@ -268,8 +322,22 @@ public class RobotContainer {
     Constants.coralSpeed.speed = Preferences.getDouble("L2 & L3 Scoring Speed", 0.45);
     Constants.coralSpeed.troughLeft = Preferences.getDouble("Trough Left Speed", 0.15);
     Constants.coralSpeed.troughRight = Preferences.getDouble("Trough Right Speed", 0.45);
+    Constants.coralSpeed.reverseSpeed = Preferences.getDouble("Coral Reverse Speed", 0.1);
 
     Constants.algaeSpeed.intakeSpeed = Preferences.getDouble("Algae Intake Speed", 1.0);
     Constants.algaeSpeed.extakeSpeed = Preferences.getDouble("Algae Extake Speed", -1.0);
+  }
+
+  // Creates controller rumble command
+  private Command controllerRumbleCommand() {
+    return Commands.startEnd(
+        () -> {
+          xDriver.getHID().setRumble(RumbleType.kBothRumble, 1.0);
+          coPilot.getHID().setRumble(RumbleType.kBothRumble, 1.0);
+        },
+        () -> {
+          xDriver.getHID().setRumble(RumbleType.kBothRumble, 0.0);
+          coPilot.getHID().setRumble(RumbleType.kBothRumble, 0.0);
+        });
   }
 }
