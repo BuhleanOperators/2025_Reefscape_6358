8b9576f5e8a4b6aaca3671f96c34b039c92d55c5
Max Young <maxyoung214@gmail.com>
2025-03-27 21:12:12 -0600

    Thursday changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -53,9 +53,4 @@ public final class Constants {
     public static double intakeSpeed = 1.0;
     public static double extakeSpeed = -1.0;
   }
-
-  public class algaeSpeed{
-    public static double intakeSpeed = 1.0;
-    public static double extakeSpeed = -1.0;
-  }
 }
