58ab19f3a6318052a10b30d49ba451620e055fdd
Max Young <maxyoung214@gmail.com>
2025-03-31 11:18:02 -0600

    End of Idaho Changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,7 @@ public final class Constants {
   public static final Mode currentMode = Mode.REAL;
   public static Height currentHeight = Height.HOME;
   public static AlgaeDirection currentDirection = AlgaeDirection.INTAKE;
-  public static boolean isCompetition = false; //TODO change comp mode
+  public static boolean isCompetition = true; //TODO change comp mode
 
   public enum Mode {
     /** Running on a real robot. */
